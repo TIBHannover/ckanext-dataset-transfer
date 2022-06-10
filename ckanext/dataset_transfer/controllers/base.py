f54d7eeb151ccacb56b8097bd7fa9a4ca0745ea8
pooya.oa <pooya.oladazimi@tib.eu>
2022-06-10 13:16:54 +0200

    added basic skeleton and blueprint

---
+++ ckanext/dataset_transfer/controllers/base.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+
+class BaseController():
+
+    def index():
+
+        return '0'
--- ckanext/dataset_transfer/plugin.py
+++ ckanext/dataset_transfer/plugin.py
@@ -1,9 +1,13 @@
+import imp
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from flask import Blueprint
+from ckanext.dataset_transfer.controllers.base import BaseController
 
 
 class DatasetTransferPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IBlueprint)
 
     # IConfigurer
 
@@ -12,3 +16,20 @@ class DatasetTransferPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic',
             'dataset_transfer')
+    
+
+
+    #plugin Blueprint
+
+    def get_blueprint(self):
+
+        blueprint = Blueprint(self.name, self.__module__)
+        blueprint.template_folder = u'templates'
+        blueprint.add_url_rule(
+            u'/dataset_transfer/index',
+            u'index',
+            BaseController.index,
+            methods=['GET']
+            )
+
+        return blueprint
