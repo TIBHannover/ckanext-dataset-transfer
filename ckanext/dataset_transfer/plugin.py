0c2849a72a5962d6ed2fed97c8eefc4264356a11
pooya.oa <pooya.oladazimi@tib.eu>
2022-06-10 14:41:23 +0200

    created public directory and web assets

--- ckanext/dataset_transfer/plugin.py
+++ ckanext/dataset_transfer/plugin.py
@@ -14,8 +14,7 @@ class DatasetTransferPlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'dataset_transfer')
+        toolkit.add_resource('public', 'ckanext-dataset-transfer')
     
 
 
