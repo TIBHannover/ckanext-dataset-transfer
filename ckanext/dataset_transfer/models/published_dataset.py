d8c0497fc183a383258ad5b99c9724bbd8810966
pooya.oa <pooya.oladazimi@tib.eu>
2022-06-24 12:16:38 +0200

    implemented the model

--- ckanext/dataset_transfer/models/published_dataset.py
+++ ckanext/dataset_transfer/models/published_dataset.py
@@ -1,10 +1,9 @@
 # encoding: utf-8
 
 import datetime
-from sqlalchemy import Column, Table, ForeignKey, orm
+from sqlalchemy import Column, Table
 from sqlalchemy import types as _types
-from sqlalchemy.sql.expression import false
-from ckan.model import meta, Resource, domain_object
+from ckan.model import meta, domain_object
 
 
 __all__ = [u"PublishedDataset", u"published_dataset_table"]
@@ -15,58 +14,37 @@ published_dataset_table = Table(
     Column(u"id", _types.Integer, primary_key=True, nullable=False),
     Column(u"dataset_id", _types.UnicodeText,  nullable=False),
     Column(u"doi", _types.UnicodeText, nullable=False),
-    Column(u"published_url", _types.UnicodeText),
+    Column(u"published_url", _types.UnicodeText, nullable=False),
     Column(u"publish_time", _types.DateTime, default=datetime.datetime.utcnow, nullable=False),
-    Column(u"publlished_dataset_id", _types.DateTime, default=datetime.datetime.utcnow, nullable=False),
+    Column(u"published_dataset_id",  _types.UnicodeText, nullable=False),
 )
 
 class PublishedDataset(domain_object.DomainObject):
-    def __init__(self, resource_id=None, sample_url=None, sample_name=None, create_at=None, updated_at=None):
-        self.resource_id = resource_id
-        self.sample_url = sample_url
-        self.sample_name = sample_name
-        self.create_at = create_at
-        self.updated_at = updated_at
+    def __init__(self, dataset_id=None, doi=None, published_url=None, publish_time=None, published_dataset_id=None):
+        self.dataset_id = dataset_id
+        self.doi = doi
+        self.published_url = published_url
+        self.publish_time = publish_time
+        self.published_dataset_id = published_dataset_id
     
-    @classmethod
-    def get_by_resource_sample(cls, id, sample_url, autoflush=True):
-        if not id:
-            return None
-
-        exists = meta.Session.query(cls).filter(cls.resource_id==id, cls.sample_url == sample_url).first() is not None
-        if not exists:
-            return False
-        query = meta.Session.query(cls).filter(cls.resource_id==id)
-        query = query.autoflush(autoflush)
-        record = query.first()
-        return record
     
 
     @classmethod
-    def get_by_resource(cls, id, autoflush=True):
+    def get_by_dataset(cls, id, autoflush=True):
         if not id:
             return None
 
-        exists = meta.Session.query(cls).filter(cls.resource_id==id).first() is not None
+        exists = meta.Session.query(cls).filter(cls.dataset_id==id).first() is not None
         if not exists:
             return False
-        query = meta.Session.query(cls).filter(cls.resource_id==id)
+        query = meta.Session.query(cls).filter(cls.dataset_id==id)
         query = query.autoflush(autoflush)
         record = query
         return record
 
-    
-    def get_resource(self):
-        return self.resource
-
 
 
 meta.mapper(
-    ResourceSampleLink,
-    resource_sample_link_table,
-    properties={
-        u"resource": orm.relation(
-            Resource, backref=orm.backref(u"resource_sample_link", cascade=u"all, delete, delete-orphan")
-        )
-    },
+    PublishedDataset,
+    published_dataset_table
 )
